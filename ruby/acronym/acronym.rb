a2c7b1e662d6bd5e5b3fdf9746bd0f8ae1cc04f5
RollerCoasterRoss <rsw3020@gmail.com>
2020-02-16 15:20:26 -0600

    acronym completed

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -6,3 +6,23 @@ To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
 
+class Acronym
+  def self.abbreviate(string)
+    acronym_array = []
+
+    string.upcase!
+    string_array_1 = string.split(" ")
+    string_array_1.delete("-")
+    string_array_1.each do |word|
+      if word.match(/\W/)
+        inner_acronym_array = word.split("-")
+        inner_acronym_array.each do |word|
+          acronym_array << word[0]
+        end
+      else
+        acronym_array << word[0]
+      end
+    end
+    return acronym_array.join
+  end
+end
