9b4048d01f116761ded118bbcc94b06d40d5973b
RollerCoasterRoss <rsw3020@gmail.com>
2020-07-08 15:48:22 -0500

    completed christmas song

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -6,64 +6,51 @@ To get started with TDD, see the `README.md` file in your
 `ruby/twelve-days` directory.
 =end
 
-# class TwelveDays
-#   def self.song
-  #   x1 = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
-  #   x2 = "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x3 = "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x4 = "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x5 = "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x6 = "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x7 = "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x8 = "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x9 = "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x10 = "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x11 = "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   x12 = "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
-  #   puts x1.concat("\n",x2,"\n",x3,"\n",x4,"\n",x5,"\n",x6,"\n",x7,"\n",x8,"\n",x9,"\n",x10,"\n",x11,"\n",x12)
-          
-  #   item_array = [
-  #                "and a Partidge in a Pear Tree.",
-  #                "two Turtle Doves,",
-  #                "three French Hens,",
-  #                "four Calling Birds,",
-  #                "five Golden Rings,",
-  #                "six Geese-a-Laying,",
-  #                "seven Swans-a-Swimming,",
-  #                "eight Maids-a-Milking,",
-  #                "nine Ladies Dancing,",
-  #                "ten Lords-a-Leaping,",
-  #                "eleven Pipers Piping,",
-  #                "twelve Drummers Drumming,"
-  #                ]
+class TwelveDays
+  def self.song
+    day_array = [
+                  "first",
+                  "second",
+                  "third",
+                  "fourth",
+                  "fifth",
+                  "sixth",
+                  "seventh",
+                  "eighth",
+                  "ninth",
+                  "tenth",
+                  "eleventh",
+                  "twelfth"
+                ]
+    lyric_array = [
+                  "a Partridge in a Pear Tree.",
+                  "and a Partridge in a Pear Tree.",
+                  "two Turtle Doves, ",
+                  "three French Hens, ",
+                  "four Calling Birds, ",
+                  "five Gold Rings, ",
+                  "six Geese-a-Laying, ",
+                  "seven Swans-a-Swimming, ",
+                  "eight Maids-a-Milking, ",
+                  "nine Ladies Dancing, ",
+                  "ten Lords-a-Leaping, ",
+                  "eleven Pipers Piping, ",
+                  "twelve Drummers Drumming, "
+                 ]
 
-  #   day_array = [
-  #               "first",
-  #               "second",
-  #               "third",
-  #               "fourth",
-  #               "fifth",
-  #               "sixth",
-  #               "seventh",
-  #               "eighth",
-  #               "ninth",
-  #               "tenth",
-  #               "eleventh",
-  #               "twelfth"
-  #               ]
-  #   counter = 0
+    message = ""
 
-  #   day_array.each do |day| 
-  #     if counter == 0
-  #       @final_1 = "On the #{ day } day of Christmas my true love gave to me: a Partridge in a Pear Tree."
-  #     else
-  #       final_array = item_array.select { |x| item_array.index(x) <= day_array.index(day) }
-  #       instance_variable_set('@final_2', "On the #{ day } day of Christmas my true love gave to me: #{final_array.reverse!.join}")
-  #     end
-  #     counter += 1
-  #   end
-  #   p @final_1.concat(@final_2)
-  # end
-# end
+    day_array.each do |day|
+      x = day_array.index(day)
+      if x == 0
+        message = "On the #{day} day of Christmas my true love gave to me: #{lyric_array.select{|lyric| lyric_array.index(lyric) < x + 1}.reverse.join}"
+      else
+        message.concat("On the #{day} day of Christmas my true love gave to me: #{lyric_array.select{|lyric| lyric_array.index(lyric) <= x + 1 && lyric_array.index(lyric) > 0}.reverse.join}")
+      end
+    end
+    message.gsub!(/Tree\./, "Tree\.\n\n").gsub!(/Tree\.\n\n\z/, "Tree\.\n\s")
+    return message
+  end
+end
 
-# TwelveDays.song
+TwelveDays.song
